b685d6324ce8ecc01c225f18c990dd6cb82375c2
Lucy Larsen <lucy.daniela1@gmail.com>
2025-04-26 20:26:05 -0600

    try again 101

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,35 @@
-import { getParkData } from "./parkService.mjs";
+// File: src/js/main.js
+
+import '../yellow.css';
+import { getParkData } from './parkService.mjs';
 
 const parkData = getParkData();
+
+// Update Disclaimer link
+const disclaimer = document.querySelector(".disclaimer > a");
+if (disclaimer) {
+  disclaimer.href = parkData.url;
+  disclaimer.innerHTML = parkData.fullName;
+}
+
+// Update Page Title
+document.title = parkData.fullName;
+
+// Update Hero Image
+const heroImg = document.getElementById("hero-img");
+if (heroImg) {
+  heroImg.src = parkData.images[0]?.url || "";
+  heroImg.alt = parkData.images[0]?.altText || parkData.fullName;
+}
+
+// Update Hero Content
+const heroContent = document.getElementById("hero-content");
+if (heroContent) {
+  heroContent.innerHTML = `
+    <a href="/" class="hero-banner__title">${parkData.name}</a>
+    <p class="hero-banner__subtitle">
+      <span>${parkData.designation}</span>
+      <span>${parkData.states}</span>
+    </p>
+  `;
+}
